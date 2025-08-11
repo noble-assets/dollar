d9935f0ebf877d7aa83bc60e815011865d28e3f3
John Letey <john@noble.xyz>
2025-08-11 04:36:31 -0400

    chore: pin compiler version
    Co-authored-by: stepit <48993133+0xstepit@users.noreply.github.com>

--- contracts/src/NobleDollar.sol
+++ contracts/src/NobleDollar.sol
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-pragma solidity >=0.8.0;
+pragma solidity 0.8.20;
 
 import {HypERC20} from "@hyperlane/token/HypERC20.sol";
 
